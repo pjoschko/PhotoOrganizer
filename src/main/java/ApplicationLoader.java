f391e687a81798e3f746ee5124ae22de04491514
pjoschko <philip.joschko@googlemail.com>
2022-07-28 15:28:39 +0200

    rootDir now is a contant

--- src/main/java/ApplicationLoader.java
+++ src/main/java/ApplicationLoader.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class ApplicationLoader {
 
 //    static final String rootDir="^\\\\HAPPY-CLOUD\\photos";
-    static final String rootDir="^/mnt/happy-cloud-photos";
+    static final String rootDir="/^mnt/happy-cloud-photos";
 
     public static void main(String[] args) {
         var application = new Application();
